96bacd34dbda35ce07f9207123dd22dfb1c27003
juanrodriguez2702 <juandavid.janleo03@gmail.com>
2023-11-27 16:46:43 -0500

    cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,3 +88,73 @@ const filter = function () {
 }
 
 addEventOnElem(filterBtns, "click", filter);
+
+
+
+
+
+
+
+
+// Registro de horas seleccionadas para cada día
+var horasPorDia = {};
+
+// Función para actualizar las horas disponibles según el día seleccionado
+function actualizarHorasDisponibles() {
+  var fechaInput = document.getElementById("fechaInput");
+  var horaSelect = document.getElementById("horaSelect");
+
+  // Obtener la fecha seleccionada
+  var fechaSeleccionada = fechaInput.value;
+
+  // Restaurar la lista completa de horas disponibles para el día seleccionado
+  horaSelect.innerHTML = "<option value='Select category'>Selecciona la hora</option>";
+
+  // Agregar todas las horas disponibles
+  var todasLasHoras = ["8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM"];
+
+  // Filtrar las horas ya seleccionadas para la fecha seleccionada
+  var horasSeleccionadas = horasPorDia[fechaSeleccionada] || [];
+
+  todasLasHoras.forEach(function (hora) {
+    if (!horasSeleccionadas.includes(hora)) {
+      var option = document.createElement("option");
+      option.value = hora;
+      option.text = hora;
+      horaSelect.add(option);
+    }
+  });
+}
+
+// Función para seleccionar y quitar la hora
+function seleccionarYQuitarHora() {
+  var fechaInput = document.getElementById("fechaInput");
+  var horaSelect = document.getElementById("horaSelect");
+  var horaSeleccionada = horaSelect.value;
+  var fechaSeleccionada = fechaInput.value;
+
+  if (horaSeleccionada !== "Select category") {
+    alert("Has seleccionado la hora: " + horaSeleccionada);
+
+    // Agregar la hora seleccionada al registro para la fecha seleccionada
+    if (!horasPorDia.hasOwnProperty(fechaSeleccionada)) {
+      horasPorDia[fechaSeleccionada] = [];
+    }
+    horasPorDia[fechaSeleccionada].push(horaSeleccionada);
+
+    // Quitar la hora seleccionada del select
+    horaSelect.remove(horaSelect.selectedIndex);
+
+    // Restaurar la opción predeterminada
+    var opcionPredeterminada = document.createElement("option");
+    opcionPredeterminada.value = "Select category";
+    opcionPredeterminada.text = "Selecciona la hora";
+    horaSelect.add(opcionPredeterminada);
+
+    // Limpiar los demás campos del formulario
+    document.getElementById("citaForm").reset();
+
+    // Actualizar las horas disponibles después de la selección de la fecha
+    actualizarHorasDisponibles();
+  }
+}
